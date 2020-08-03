28bd70e3fc37ba067f9b586ba1840480758deb27
Zach <zjsievers@gmail.com>
2020-08-03 13:39:25 -0400

    moved questions to json file

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
-const question = document.getElementById('question');
-const choices = Array.from(document.getElementsByClassName('choice-text'));
-const progressText = document.getElementById('progressText');
-const scoreText = document.getElementById('score');
-const progressBarFull = document.getElementById('progressBarFull');
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+const progressText = document.getElementById("progressText");
+const scoreText = document.getElementById("score");
+const progressBarFull = document.getElementById("progressBarFull");
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -10,105 +10,92 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
-let questions = [
-    {
-        question: "Who did God tell to build an Ark?",
-        choice1: "Noah",
-        choice2: "Adam",
-        choice3: "Ruth",
-        choice4: "Jesus",
-        answer: 1,
-    },
-    {
-        question: "How did David defeat Goliath?",
-        choice1: "Praying",
-        choice2: "Sword",
-        choice3: "Stone and a Sling",
-        choice4: "He didn't",
-        answer: 3,
-    },
-    {
-        question: "What did God say when he created light?",
-        choice1: "Illuminate Thee!",
-        choice2: "Let there be light",
-        choice3: "Thine lighteth turneth on",
-        choice4: "Let there be sun",
-        answer: 2,
-    },
-];
+let questions = [];
+
+fetch("questions.json")
+  .then(res => {
+    console.log(res);
+    return res.json();
+  })
+  .then(loadedQuestions => {
+    console.log(loadedQuestions);
+    questions = loadedQuestions;
+    startGame();
+  }).catch(err => {
+      console.error(err);
+  });
 
 // CONSTANTS
 const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 3;
 
 startGame = () => {
-    questionCounter = 0;
-    score = 0;
-    // take this array - spread out its items and put them into a new array.
-    availableQuestions = [...questions];
-    getNewQuestion();
+  questionCounter = 0;
+  score = 0;
+  // take this array - spread out its items and put them into a new array.
+  availableQuestions = [...questions];
+  getNewQuestion();
 };
 
 getNewQuestion = () => {
-    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+  if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    localStorage.setItem("mostRecentScore", score);
+    // go to end page
+    return window.location.assign("/end.html");
+  }
 
-        localStorage.setItem('mostRecentScore', score);
-        // go to end page
-        return window.location.assign("/end.html");
-    }
+  questionCounter++;
+  progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
 
-    questionCounter++;
-    progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+  // updating progress bar
+  progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
 
-    // updating progress bar
-    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+  const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+  currentQuestion = availableQuestions[questionIndex];
+  question.innerText = currentQuestion.question;
 
-    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
-    currentQuestion = availableQuestions[questionIndex];
-    question.innerText = currentQuestion.question;
+  choices.forEach((choice) => {
+    const number = choice.dataset["number"];
+    choice.innerText = currentQuestion["choice" + number];
+  });
 
-    choices.forEach(choice => {
-        const number = choice.dataset["number"];
-        choice.innerText = currentQuestion["choice" + number];
-    });
+  availableQuestions.splice(questionIndex, 1);
 
-    availableQuestions.splice(questionIndex, 1);
-
-    acceptingAnswers = true;
+  acceptingAnswers = true;
 };
 
-choices.forEach(choice => {
-    choice.addEventListener('click', e => {
-        if(!acceptingAnswers) return;
+choices.forEach((choice) => {
+  choice.addEventListener("click", (e) => {
+    if (!acceptingAnswers) return;
 
-        acceptingAnswers = false;
-        const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset["number"];
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
 
-        // const classToApply = 'incorrect';
-        // if (selectedAnswer == currentQuestion.answer) {
-        //     classToApply = 'correct';
-        // }; 
+    // const classToApply = 'incorrect';
+    // if (selectedAnswer == currentQuestion.answer) {
+    //     classToApply = 'correct';
+    // };
 
-        // ternary operator
-        const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
-        
-        if(classToApply === 'correct') {
-            incrementScore(CORRECT_BONUS);
-        }
+    // ternary operator
+    const classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
 
-        selectedChoice.parentElement.classList.add(classToApply);
+    if (classToApply === "correct") {
+      incrementScore(CORRECT_BONUS);
+    }
 
-        setTimeout(() => {
-            selectedChoice.parentElement.classList.remove(classToApply);
-            getNewQuestion();
-        }, 1000);       
-    });
+    selectedChoice.parentElement.classList.add(classToApply);
+
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1000);
+  });
 });
 
-incrementScore = num => {
-    score += num; 
-    scoreText.innerText = score
-}
+incrementScore = (num) => {
+  score += num;
+  scoreText.innerText = score;
+};
 
-startGame();
