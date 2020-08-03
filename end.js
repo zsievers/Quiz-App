0b3f0c1c07793a8a2e8f34362f35dd83713a0df0
Zach <zjsievers@gmail.com>
2020-08-03 12:44:22 -0400

    push score into array, sort, splice to create a top 5 list

--- end.js
+++ end.js
@@ -2,13 +2,36 @@ const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const MAX_HIGH_SCORES = 5;
+console.log(highScores);
+
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup',  () => {
     saveScoreBtn.disabled = !username.value;
 });
 
-saveHighScore = () => {
-    console.log("clicked teh save butn");
+saveHighScore = e => {
+    console.log("clicked the save butn");
     e.preventDefault();
-}
+
+    const score = {
+        score: Math.floor(Math.random() * 100),
+        name: username.value
+    };
+    highScores.push(score);
+
+    // sorting scores to make a highscore list
+    highScores.sort((a,b) => b.score - a.score)
+
+    // splicing at 5th index - cutting off highscores after 5
+    highScores.splice(5);
+
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+
+    window.location.assign("/");
+    console.log(highScores);
+};
