2e24419d58a5321852106bf901c760ba036e7e47
Zach <zjsievers@gmail.com>
2020-08-04 12:23:56 -0400

    added trivia db api instead of hard coded questions

--- game.js
+++ game.js
@@ -12,17 +12,37 @@ let availableQuestions = [];
 
 let questions = [];
 
-fetch("questions.json")
-  .then(res => {
-    console.log(res);
+fetch(
+  "https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple"
+)
+  .then((res) => {
     return res.json();
   })
-  .then(loadedQuestions => {
-    console.log(loadedQuestions);
-    questions = loadedQuestions;
+  .then((loadedQuestions) => {
+    console.log(loadedQuestions.results);
+    questions = loadedQuestions.results.map((loadedQuestion) => {
+      const formattedQuestion = {
+        question: loadedQuestion.question,
+      };
+
+      const answerChoices = [...loadedQuestion.incorrect_answers];
+      formattedQuestion.answer = Math.floor(Math.random() * 3) + 1;
+      answerChoices.splice(
+        formattedQuestion.answer - 1,
+        0,
+        loadedQuestion.correct_answer
+      );
+
+      answerChoices.forEach((choice, index) => {
+        formattedQuestion["choice" + (index + 1)] = choice;
+      });
+
+      return formattedQuestion;
+    });
     startGame();
-  }).catch(err => {
-      console.error(err);
+  })
+  .catch((err) => {
+    console.error(err);
   });
 
 // CONSTANTS
@@ -98,4 +118,3 @@ incrementScore = (num) => {
   score += num;
   scoreText.innerText = score;
 };
-
