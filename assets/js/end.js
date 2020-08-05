879ba956499cf3482c45e1c15309d64a8cc9a9b8
Zach <zjsievers@gmail.com>
2020-08-05 13:28:19 -0400

    adjusted css path

---
+++ assets/js/end.js
@@ -0,0 +1,35 @@
+const username = document.getElementById("username");
+const saveScoreBtn = document.getElementById("saveScoreBtn");
+const finalScore = document.getElementById("finalScore");
+const mostRecentScore = localStorage.getItem("mostRecentScore");
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const MAX_HIGH_SCORES = 5;
+
+finalScore.innerText = mostRecentScore;
+
+username.addEventListener("keyup", () => {
+  saveScoreBtn.disabled = !username.value;
+});
+
+saveHighScore = (e) => {
+  e.preventDefault();
+
+  const score = {
+    score: mostRecentScore,
+    name: username.value,
+  };
+  highScores.push(score);
+
+  // sorting scores to make a highscore list
+  highScores.sort((a, b) => b.score - a.score);
+
+  // splicing at 5th index - cutting off highscores after 5
+  highScores.splice(5);
+
+  localStorage.setItem("highScores", JSON.stringify(highScores));
+
+  window.location.assign("/");
+  console.log(highScores);
+};
